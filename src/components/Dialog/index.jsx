4124cefb620f5d79f2d67328aa8cf6e72ca51b77
Dudsnavarro <eduardo.dudunavarro@oultook.com>
2026-03-31 09:22:26 -0300

    Base do projeto + dialog

---
+++ src/components/Dialog/index.jsx
@@ -0,0 +1,24 @@
+import { useRef } from "react";
+
+export function Dialog() {
+
+  const dialogRef = useRef (null)
+
+  const openDialog = () => {
+    dialogRef.current.showModal();
+  };
+
+  const closeDialog = () => {
+    dialogRef.current.close();
+  };
+
+  return (
+    <React.Fragment>
+      <dialog ref={dialogRef}>
+        <button autoFocus onClick={closeDialog}>Close</button>
+        <p>This modal dialog has a groovy backdrop!</p>
+      </dialog>
+      <button onClick={openDialog}>Show the dialog</button>
+    </React.Fragment>
+  );
+}
