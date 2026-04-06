a08f79b8fcc61df297e6168242dd83a746d062d3
Dudsnavarro <eduardo.dudunavarro@oultook.com>
2026-04-06 09:35:04 -0300

    Funcionalidade de adição e remoção

--- src/App.jsx
+++ src/App.jsx
@@ -12,49 +12,21 @@ import { ToDoList } from "./components/ToDoList"
 import { Dialog } from "./components/Dialog"
 import { ToDoForm } from "./components/ToDoForm"
 
-const todos = [
-  {
+function App() {
+
+  const [todos, setTodos] = useState([
+      {
     id: 1,
-    description: "JSX e componentes",
+    description: "as e componentesa",
     completed: false,
     createdAt: "2022-10-31"
-  },
-  {
+  },  {
     id: 2,
-    description: "Props, state e hooks",
-    completed: false,
-    createdAt: "2022-10-31"
-  },
-  {
-    id: 3,
-    description: "Ciclo de vida dos componentes",
-    completed: false,
-    createdAt: "2022-10-31"
-  },
-  {
-    id: 4,
-    description: "Testes unitários com Jest",
-    completed: false,
-    createdAt: "2022-10-31"
-  }
-]
-const completed = [
-  {
-    id: 5,
     description: "Controle de inputs e formulários controlados",
     completed: true,
     createdAt: "2022-10-31"
   },
-  {
-    id: 6,
-    description: "Rotas dinâmicas",
-    completed: true,
-    createdAt: "2022-10-31"
-  }
-]
-
-function App() {
-
+  ])
   const [showDialog, setShowDialog] = useState(false)
 
   const toggleDialog = () => {
@@ -62,9 +34,40 @@ function App() {
 
   }
 
-  const addTodo = () => {
-    console.log("Adicionar todo")
+  const addTodo = (formaData) => {
+    const description = formaData.get('description')
+    setTodos(
+      prevState => {
+        const todo = {
+          id: prevState.length + 1,
+          description: description,
+          completed: false,
+          createdAt: new Date().toISOString()          
+        }
+        return [...prevState, todo]
+      }
+    )
     toggleDialog()
+  }
+
+  const toggleTodoCompleted = (todo) => {
+    setTodos(prevState => {
+      return prevState.map(t => {
+        if (t.id == todo.id) {
+          return {
+            ...t,
+            completed: !t.completed
+          }
+        }
+        return t
+      })
+    })
+  }
+
+  const deleteTodo = (todo) => {
+    setTodos(prevState => {
+      return prevState.filter( t => t.id != todo.id)
+    })
   }
 
   return (
@@ -78,14 +81,24 @@ function App() {
         <ChecklistsWrapper>
           <SubHeading>Para estudar</SubHeading>
           <ToDoList>
-            {todos.map(function (t) {
-              return <ToDoItem key={t.id} item={t} />
+            {todos.filter(t => !t.completed).map(function (t) {
+                return <ToDoItem 
+                key={t.id} 
+                item={t} 
+                ontoggleTCompleted={toggleTodoCompleted}
+                onDeleteTodo={deleteTodo}
+              />
             })}
           </ToDoList>
           <SubHeading>Concluído</SubHeading>
           <ToDoList>
-            {completed.map(function (t) {
-              return <ToDoItem key={t.id} item={t} />
+            {todos.filter(t => t.completed).map(function (t) {
+              return <ToDoItem 
+                key={t.id} 
+                item={t} 
+                ontoggleTCompleted={toggleTodoCompleted}
+                onDeleteTodo={deleteTodo}
+              />
             })}
           </ToDoList>
           <Footer>
